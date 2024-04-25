7ab675ab78f0f9f534d2cf942976cd87ce232fab
Jameson Andrews <jamesonandrews@jamesons-laptop.hamilton.edu>
2024-04-25 16:11:00 -0400

    fix table call

--- cents.py
+++ cents.py
@@ -10,11 +10,16 @@ def rounding(price):
 
 
 def recursive_cents(table, items, dividers, prices, last_divide):
+    
     total = 0
+    print(dividers, items)
+    if table[dividers][items - 1] != sys.maxsize:
+        return table[dividers][items - 1]
+    
     if items == 0 or dividers == 0:
         total = rounding(sum(prices[:last_divide]))
-        # table[dividers][items] = total
-        table[i] = total
+        table[dividers][items - 1] = total
+        # table[i] = total
         return total
      
     total = sys.maxsize
@@ -25,22 +30,22 @@ def recursive_cents(table, items, dividers, prices, last_divide):
         dontdivide = recursive_cents(table, items - 1, dividers, prices, last_divide + 1)
         total = min(total, usedivide, dontdivide)
         total = rounding(total)
-        # table[dividers][i] = total
         table[dividers][i] = total
+        # table[dividers][i] = total
     return total
 
 def cents(items, dividers, prices):
     
 
-    # table = []
-    # for i in range(dividers + 1):
-    #     line = []
-    #     for j in range(items):
-    #         line.append(None)
-    #     table.append(line)
+    table = []
+    for i in range(dividers + 1):
+        line = []
+        for j in range(items):
+            line.append(sys.maxsize)
+        table.append(line)
 
 
-    table = [sys.maxsize] * items
+    # table = [sys.maxsize] * items
 
     print (table)
     cost = recursive_cents(table, items, dividers, prices, items)
@@ -48,12 +53,12 @@ def cents(items, dividers, prices):
     print (cost)
 
 def main():
-    # items = 3
-    # dividers = 1
-    # prices = [13, 21, 54]
-    items = 5
-    dividers = 2
-    prices = [1, 1, 1, 1, 1]
+    items = 3
+    dividers = 1
+    prices = [13, 21, 54]
+    # items = 5
+    # dividers = 2
+    # prices = [1, 1, 1, 1, 1]
     cents(items, dividers, prices)
 
 main()
