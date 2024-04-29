7965b29f168d8b55228a07e97d57f8d16505a879
Jameson Andrews <jamesonandrews@jamesons-laptop.hamilton.edu>
2024-04-29 15:43:02 -0400

    ugh

--- cents.py
+++ cents.py
@@ -9,6 +9,55 @@ def rounding(price):
     return price
 
 
+
+def recursive_ents(table, start, dividers, prices, last_divide):
+    total = 0
+    if table[dividers][start] != sys.maxsize:
+        return table[dividers][start]
+
+    if start == len(prices) or dividers == 0:
+        total = rounding(sum(prices[last_divide:]))
+        table[dividers][len(prices)] = total
+        # print("dividers: ", dividers, "items: ", items, "total: ", total)
+        return total
+    
+    total = sys.maxsize
+    usedivide = 0
+    dontdivide = 0
+    for i in range(start, len(prices)):
+        usedivide = recursive_cents(table, i, dividers - 1, prices, i) + rounding(sum(prices[last_divide:i]))
+        dontdivide = recursive_cents(table, i + 1, dividers, prices, last_divide)
+        total = min(total, usedivide, dontdivide)
+        total = rounding(total)
+        table[dividers][i] = total
+        # print("dividers: ", dividers, "items: ", i, "total: ", total)
+    return total
+
+# def recursive_cents(table, items, dividers, prices, last_divide):
+
+#     total = 0
+#     # print(dividers, items)
+#     if table[dividers][items] != sys.maxsize:
+#         return table[dividers][items]
+    
+#     if items == 0 or dividers == 0:
+#         total = rounding(sum(prices[:last_divide]))
+#         table[dividers][items] = total
+#         print("dividers left: ", dividers, "items: ", items, "total: ", total)
+#         return total
+     
+#     total = sys.maxsize
+#     usedivide = 0
+#     dontdivide = 0
+#     for i in range(items, -1, -1):
+#         usedivide = recursive_cents(table, i - 1, dividers - 1, prices, i) + rounding(sum(prices[i:last_divide]))
+#         dontdivide = recursive_cents(table, i - 1 , dividers, prices, last_divide)
+#         total = min(total, usedivide, dontdivide)
+#         total = rounding(total)
+#         table[dividers][i] = total
+#         print("dividers left: ", dividers, "items: ", i, "total: ", total)
+#     return total
+
 def recursive_cents(table, items, dividers, prices, last_divide):
 
     total = 0
@@ -16,27 +65,33 @@ def recursive_cents(table, items, dividers, prices, last_divide):
     if table[dividers][items] != sys.maxsize:
         return table[dividers][items]
     
-    if items == 0 or dividers == 0:
-        total = rounding(sum(prices[:last_divide]))
+    if items == 0 or (dividers == 0 and items >= 1):
+        total = rounding(sum(prices[last_divide :]))
         table[dividers][items] = total
-        # table[i] = total
+        # print("dividers left: ", dividers, "items: ", items, "total: ", total)
+        # pretty_print(table)
         return total
      
     total = sys.maxsize
     usedivide = 0
     dontdivide = 0
-    for i in range(items - 1 , -1, -1):
-        usedivide = recursive_cents(table, items - 1, dividers - 1, prices, i) + rounding(sum(prices[i:last_divide]))
-        dontdivide = recursive_cents(table, items -1 , dividers, prices, last_divide)
+    for i in range(items, -1, -1):
+        dontdivide = recursive_cents(table, i - 1 , dividers, prices, last_divide)
+        usedivide = recursive_cents(table, i - 1, dividers - 1, prices, i) + rounding(sum(prices[last_divide : i]))
         total = min(total, usedivide, dontdivide)
         total = rounding(total)
         table[dividers][i] = total
-        # table[i] = total
+        # print("dividers left: ", dividers, "items: ", i, "total: ", total)
+        # pretty_print(table)
     return total
 
-def cents(items, dividers, prices):
-    
 
+
+def pretty_print(table):
+    for i in range(len(table)):
+        print(table[i])
+
+def cents(items, dividers, prices):
     table = []
     for i in range(dividers + 1):
         line = []
@@ -44,20 +99,16 @@ def cents(items, dividers, prices):
             line.append(sys.maxsize)
         table.append(line)
 
-
-    # table = [sys.maxsize] * items
-
-    # print (table)
-    cost = recursive_cents(table, items, dividers, prices, items)
-    # print(table)
+    cost = recursive_cents(table, items, dividers, prices, 0)
+    # cost = recursive_cents(table, 0, dividers, prices, 0)
+    # print()
+    # pretty_print(table)
+    # print()
     print (cost)
 
 def main():
     items, dividers  = map(int, input().split())
     prices = [int(x) for x in sys.stdin.readline().split()]
-    # print (items)
-    # print(dividers)
-    # print (prices)
     cents(items, dividers, prices)
 
 if __name__ == "__main__":
