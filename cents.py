85bdcf49cf5df7f388eca02f1cf9cd19a7759e5f
Jameson Andrews <jamesonandrews@jamesons-laptop.hamilton.edu>
2024-04-29 11:37:50 -0400

    good luck kai

--- cents.py
+++ cents.py
@@ -10,15 +10,15 @@ def rounding(price):
 
 
 def recursive_cents(table, items, dividers, prices, last_divide):
-    
+
     total = 0
-    print(dividers, items)
-    if table[dividers][items - 1] != sys.maxsize:
-        return table[dividers][items - 1]
+    # print(dividers, items)
+    if table[dividers][items] != sys.maxsize:
+        return table[dividers][items]
     
     if items == 0 or dividers == 0:
         total = rounding(sum(prices[:last_divide]))
-        table[dividers][items - 1] = total
+        table[dividers][items] = total
         # table[i] = total
         return total
      
@@ -27,11 +27,11 @@ def recursive_cents(table, items, dividers, prices, last_divide):
     dontdivide = 0
     for i in range(items - 1 , -1, -1):
         usedivide = recursive_cents(table, items - 1, dividers - 1, prices, i) + rounding(sum(prices[i:last_divide]))
-        dontdivide = recursive_cents(table, items - 1, dividers, prices, last_divide + 1)
+        dontdivide = recursive_cents(table, items -1 , dividers, prices, last_divide)
         total = min(total, usedivide, dontdivide)
         total = rounding(total)
         table[dividers][i] = total
-        # table[dividers][i] = total
+        # table[i] = total
     return total
 
 def cents(items, dividers, prices):
@@ -40,25 +40,25 @@ def cents(items, dividers, prices):
     table = []
     for i in range(dividers + 1):
         line = []
-        for j in range(items):
+        for j in range(items + 1):
             line.append(sys.maxsize)
         table.append(line)
 
 
     # table = [sys.maxsize] * items
 
-    print (table)
+    # print (table)
     cost = recursive_cents(table, items, dividers, prices, items)
-    print(table)
+    # print(table)
     print (cost)
 
 def main():
-    items = 3
-    dividers = 1
-    prices = [13, 21, 54]
-    # items = 5
-    # dividers = 2
-    # prices = [1, 1, 1, 1, 1]
+    items, dividers  = map(int, input().split())
+    prices = [int(x) for x in sys.stdin.readline().split()]
+    # print (items)
+    # print(dividers)
+    # print (prices)
     cents(items, dividers, prices)
 
-main()
+if __name__ == "__main__":
+    main()
